c00d006fe5ff37549dee45a1332e716b88629c8c
EthanUmmel <94692709+EthanUmmel@users.noreply.github.com>
2022-08-30 16:28:30 +0200

    Exercice partiellement terminé

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,14 +1,13 @@
 /**
 * Jeu du nombre mystère
-* @author  Steve Fallet
-* @version 0.1
-* @since   2018-09-09 (date de création)
+* @author  Ethan Ummel
+* @version 0.2
+* @since   30.08.2022
 */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
-
+    'use strict';
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
      * @param min
@@ -18,5 +17,26 @@
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
+    let nbMystere;
+    let nbEssai = 0;
+    let message = ``;
+
+    nbMystere = tireNombre(1, 100);
+
+    let reponse = Number(prompt('Entrez un nombre entre 1 et 100'));
+    do {
+        nbEssai++;
+        if (reponse > nbMystere) {
+            message = `C'est moins !`;
+            prompt(message);
+        } else if (reponse > nbMystere) {
+            message = `C'est plus !`;
+            prompt(message);
+        }
+    } while (reponse === nbMystere);
+
+    alert(`Vous avez trouvé après ${nbEssai} essais.`);
+
+
 
 }()); // main IIFE
